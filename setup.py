159a202c923eb6222d74e3b42584c517a9262cf1
Damaz de Jong <damaz.de.jong@planet.nl>
2016-11-02 22:22:24 +0100

    add contents to setup.py

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='pysweep',
+      version='0.1',
+      description='Sweep functions for qcodes',
+      url='https://github.com/damazter/pysweep',
+      author='Damaz de Jong',
+      author_email='damaz.de.jong@planet.nl',
+      license='Private',
+      packages=['pysweep'],
+      zip_safe=False)
