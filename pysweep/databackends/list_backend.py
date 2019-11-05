99af8f3463a86ec5d0e23f26051d5818421a6ee7
Administrator <d.dejong@tudelft.nl>
2019-11-05 16:11:31 +0100

    start working on an example notebook

---
+++ pysweep/databackends/list_backend.py
@@ -0,0 +1,25 @@
+import pysweep.databackends.base as base
+
+class ListDataBackend(base.DataBackend, base.DataSaver):
+    def __init__(self):
+        self.paramstruct = None
+        self.lines = []
+
+    def setup(self, paramstructure):
+        self.paramstruct = paramstructure
+
+    def __enter__(self):
+        return self
+
+    def __exit__(self, exc_type, exc_val, exc_tb):
+        pass
+
+    def add_to_line(self, line):
+        self.lines.append(line)
+
+    def write_line(self):
+        pass
+
+    def write_block(self):
+        pass
+
